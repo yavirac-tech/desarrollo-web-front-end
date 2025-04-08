a5022c5d7f7992b04eb19e5d6a8253d06e60af13
cmtl0204 <cesar.tamayo0204@gmail.com>
2025-04-07 19:53:28 -0500

    add if, switch

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {InputText} from 'primeng/inputtext';
 import {FormsModule} from '@angular/forms';
+import {ProductInterface} from '../list/list.component';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-form',
@@ -15,4 +17,19 @@ export class FormComponent {
   protected name: string = 'Mauricio';
   protected lastname: string = 'Tamayo';
   protected day: string = '1';
+  protected products: ProductInterface[] = [];
+  private httpClient = inject(HttpClient);
+  private url = 'https://67ef0ef3c11d5ff4bf7ba62a.mockapi.io/api/v1/products';
+  protected numeros: string[] = ['true','false'];
+
+  constructor() {
+    // this.findAll();
+  }
+
+  findAll() {
+    const request = this.httpClient.get<ProductInterface[]>(this.url);
+    request.subscribe(response => {
+      this.products = response;
+    });
+  }
 }
